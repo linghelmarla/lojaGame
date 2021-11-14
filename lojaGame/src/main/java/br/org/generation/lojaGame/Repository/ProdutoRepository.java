e19605496e083f3925dfd02e03415b8263a30a17
linghel marla <linghel.marlacms@gmail.com>
2021-11-14 15:52:13 -0300

    refazendo exercicio

---
+++ lojaGame/src/main/java/br/org/generation/lojaGame/Repository/ProdutoRepository.java
@@ -0,0 +1,21 @@
+package br.org.generation.lojaGame.Repository;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.org.generation.lojaGame.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+	 
+	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+	
+	 
+	public List <Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+	
+}
---
+++ lojaGame/src/test/java/br/org/generation/lojaGame/LojaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.lojaGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
